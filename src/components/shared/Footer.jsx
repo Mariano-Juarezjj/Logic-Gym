637b1e595a76acddb8456d6372c30c6d31b31cc7
Diego Giménez <gimenezd15@gmail.com>
2026-06-25 20:18:07 -0300

    Le agregue estilos al footer y agregue la importacion de bootstrap en Main

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import logoLogicGym from "../../assets/logoLogicGym.png";
 import { FaFacebook, FaWhatsapp, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import "../../App.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <div>
-          <div>
+    <footer className="footer">
+      <div className="container">
+        <div className="row gy-4">
+          <div className="col-lg-3 col-md-6">
             <img
               src={logoLogicGym}
               alt="Logo Logic Gym"
               className="footerLogo"
             />
-            <p>
+            <p className="descripcionFooter mt-3">
               Donde el entrenamiento fisico y la programacion se unen para
               desarrollar la mejor version de vos mismo
             </p>
@@ -22,34 +23,34 @@ const Footer = () => {
               <p>3816042407</p>
             </a>
 
-            <div>
-              <a href="">
+            <div className="redesSociales">
+              <a href="" className="wsp">
                 <FaWhatsapp />
               </a>
-              <a href="">
+              <a href="" className="instagram">
                 <FaInstagram />
               </a>
-              <a href="">
+              <a href="" className="facebook">
                 <FaFacebook />
               </a>
-              <a href="">
+              <a href="" className="x">
                 <FaXTwitter />
               </a>
             </div>
           </div>
 
-          <div>
-            <h4>Nuestra Historia</h4>
-            <p>
+          <div className="col-lg-3 col-md-6">
+            <h4 className="titulo">Nuestra Historia</h4>
+            <p className="descripcionFooter">
               Logic Gym nace con la idea de combinar disciplina física y mental.
               Creemos que el cuerpo y la lógica se entrenan todos los días,
               dentro y fuera del gimnasio.
             </p>
           </div>
 
-          <div>
-            <h4>Explorar</h4>
-            <div>
+          <div className="col-lg-2 col-md-6">
+            <h4 className="titulo">Explorar</h4>
+            <div className="linksNavegacion">
               <a href="">Planes</a>
               <a href="">Sedes</a>
               <a href="">Información</a>
@@ -57,8 +58,8 @@ const Footer = () => {
             </div>
           </div>
 
-          <div>
-            <h4>Ubicación</h4>
+          <div className="col-lg-4 col-md-6">
+            <h4 className="titulo">Ubicación</h4>
             <iframe
               src={
                 "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.1060679495154!2d-65.20974192576678!3d-26.836578490024703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1782311797592!5m2!1ses-419!2sar"
@@ -74,9 +75,9 @@ const Footer = () => {
           </div>
         </div>
 
-        <div>
+        <div className="botonCopy">
           <p>
-            © {new Date().getFullYear()} Logic Gym - Todos los derechos
+            &copy; {new Date().getFullYear()} Logic Gym - Todos los derechos
             reservados{" "}
           </p>
         </div>
@@ -86,5 +87,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-
