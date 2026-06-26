9e7161c3c1ece608ef4dee7d5e1cb9855db8a34a
Mariano Juarez <nanito.mj@gmail.com>
2026-06-26 15:00:44 -0300

    se agrego la ubicacion con el mapa de google maps al componente contacto

--- src/components/views/Contacto.jsx
+++ src/components/views/Contacto.jsx
@@ -49,6 +49,27 @@ const Contacto = () => {
               </Button>
             </Form>
           </div>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} md={8} lg={6}>
+          
+          <div className="text-center mb-4">
+            <h3 className="mapa-titulo">Nuestra Ubicación</h3>
+          </div>
+
+          <div className="mapa-contenedor p-2">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d920.9054515084337!2d-65.20700496289064!3d-26.836837074287967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e1!3m2!1ses-419!2sar!4v1782496651508!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"
+              width="100%"
+              height="350"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="Ubicación de Logic Gym"
+            ></iframe>
+          </div>
 
         </Col>
       </Row>
