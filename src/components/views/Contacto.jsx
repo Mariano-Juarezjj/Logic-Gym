61e829ee746647871f08a00463dcccb762636635
Mariano Juarez <nanito.mj@gmail.com>
2026-06-26 15:53:19 -0300

    se añadieron validaciones en el componente contacto

--- src/components/views/Contacto.jsx
+++ src/components/views/Contacto.jsx
@@ -1,9 +1,21 @@
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { Link, useNavigate } from "react-router";
+import { useForm } from "react-hook-form"
 import Swal from "sweetalert2";
 import "../../App.css";
 
 const Contacto = () => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log("Datos del formulario validados", data);
+    }
+
   return (
     <Container className="contacto-wrapper py-5">
       <Row className="justify-content-center">
@@ -15,14 +27,21 @@ const Contacto = () => {
           </div>
 
           <div className="contacto-caja p-4 p-md-5">
-            <Form>
+            <Form onSubmit={handleSubmit(onSubmit)}>
               <Form.Group className="mb-4" controlId="formNombre">
                 <Form.Label className="contacto-label">Nombre completo</Form.Label>
                 <Form.Control 
                   type="text" 
                   placeholder="Ej: Juan Pérez" 
-                  className="contacto-input" 
+                  className="contacto-input"
+                  {...register("nombre", {
+                    required: "El nombre es un dato obligatorio",
+                    
+                  })} 
                 />
+                <Form.Text className="text-danger">
+                    {errors.nombre?.message}
+                </Form.Text>
               </Form.Group>
 
               <Form.Group className="mb-4" controlId="formEmail">
@@ -31,7 +50,17 @@ const Contacto = () => {
                   type="email" 
                   placeholder="tu@email.com" 
                   className="contacto-input" 
+                  {...register("email", {
+                    required: "El email es un dato obligatorio",
+                    pattern: {
+                        value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                        message: "El mail debe ser un correo valido por ej: juanperez@mail.com"
+                    }
+                  })} 
                 />
+                <Form.Text className="text-danger">
+                    {errors.email?.message}
+                </Form.Text>
               </Form.Group>
 
               <Form.Group className="mb-4" controlId="formMensaje">
@@ -40,8 +69,22 @@ const Contacto = () => {
                   as="textarea" 
                   rows={4} 
                   placeholder="¿En qué te podemos ayudar?" 
-                  className="contacto-input" 
+                  className="contacto-input"
+                  {...register("mensaje", {
+                    required: "El mensaje es un dato obligatorio",
+                    minLength: {
+                        value: 20,
+                        message: "El mensaje debe tener al menos 20 caracteres",
+                    },
+                    maxLength: {
+                        value: 150,
+                        message: "El mensaje debe tener como maximo 150 caracteres"
+                    }
+                  })} 
                 />
+                <Form.Text className="text-danger">
+                    {errors.mensaje?.message}
+                </Form.Text>
               </Form.Group>
 
               <Button type="submit" className="contacto-btn w-100 mt-2">
